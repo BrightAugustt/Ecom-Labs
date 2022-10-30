50707ab70276f05268a6465fb4ce7e5b8e945049
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-10-30 21:43:21 +0000

    a

--- Login/loginprocess.php
+++ Login/loginprocess.php
@@ -1,46 +1,43 @@
 <?php
 include("../controllers/customer_controller.php");
 
-
-$customer_email = $_POST["cemail"];
-    $customer_pass = $_POST["cpass"];
-
+// Collecting form data
+// GET is used by default so define the type of method
+// POST is used for secure information transmission.
 if(isset($_POST["login"])){
-    
-    
-    
-    if(selectOne_Email_ctr($customer_email)!=false){
-        $verify = selectOne_Email_ctr($customer_email);
-        $pass = $verify["customer_pass"];
-
-        if (password_verify($customer_pass2,$customer_pass) and $check['user_role']==1){
-            session_start();
-            $_SESSION['customer_id'] = $result['customer_id'];
-            $_SESSION['custmer_name'] = $result['customer_name'];
-            $_SESSION['customer_email'] = $result['customer_email'];
-
-            header('Location:../Admin/admin.php');
-        }
-
-        else if (password_verify($customer_pass2,$customer_pass) and $check['user_role']!=1) {
-            session_start();
-            $_SESSION['customer_id'] = $result['customer_id'];
-            $_SESSION['custmer_name'] = $result['customer_name'];
-            $_SESSION['customer_email'] = $result['customer_email'];
-            header('Location:../view/home.php');
-        } else{
-            session_start();
-            $_SESSION['error'] = 'Invalid login details!';    
-            header('Location:../Login/login.php');
-        }
 
 
-    }
-}
+    // Variable to capture the name value for each user input.
+    $customer_email = $_POST["cemail"];
+    $customer_pass = $_POST["cpass"];
+    $login =loginCustomer_ctr($customer_email);
+    $verify = selectOne_Email_ctr($customer_email);
+
+        if($login != false){
+            if (password_verify($customer_pass,$login['customer_pass']) and $verify['user_role']==1 ){
+
+                      
+                       $_SESSION['id']= $login['customer_pass'];
+                         // redirect to login
+                            header('Location:../index.php');
+            }
+
+            
+            else{
+                session_start();
+$_SESSION['error']='Invalid credentials';
+                header('Location:login.php');
+            }
+                    }
+                    else{
+                        session_start();
+$_SESSION['error']='Invalid credentials';
+                        header('Location:login.php');
+                    }
         
 
 
-
+    }
 
 
 ?>
