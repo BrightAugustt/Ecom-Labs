10adb8878f336fee71714a1df8f93f5dcb500a33
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-10-30 23:09:57 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,8 +1,8 @@
 <?php
 //Database credentials
-Define('SERVER', 'localhost');
-Define('USERNAME', 'root');
-Define('PASSWD', '');
-Define('DATABASE', 'shoppn');
+Define('SERVER', 'us-cdbr-east-06.cleardb.net');
+Define('USERNAME', 'b8381e8687357a');
+Define('PASSWORD', '246b0cb1');
+Define('DATABASE', 'heroku_c41210addaccbb7');
 
 ?>
