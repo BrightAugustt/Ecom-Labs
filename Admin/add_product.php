ee6d6114e942618cd1055a63d4f0b9181d2b017e
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-11-06 20:37:44 +0000

    aaa

--- Admin/add_product.php
+++ Admin/add_product.php
@@ -17,7 +17,7 @@ if (empty($_SESSION['customer_id']) and empty($_SESSION['customer_name']) and em
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Trial</title>
+        <title>Add Product Page</title>
         <link rel="stylesheet" href="stylesc.css">
         <link rel="stylesheet" href="mediaQueries.css">
     </head>
