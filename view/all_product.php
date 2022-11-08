919f37a2e1aca68afdfe21b59ca196e0e0288e4b
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-11-08 23:12:10 +0000

    aa

--- view/all_product.php
+++ view/all_product.php
@@ -58,14 +58,12 @@ if (empty($_SESSION['customer_id']) and empty($_SESSION['customer_name']) and em
            $product =  selectall_product_ctr();  
             foreach($product as $item){ 
         ?>
-        <div class="input-group">
-        <div class="form-outline">
-            <input type="search" id="form1" class="form-control" />
-            <label class="form-label" for="form1">Search</label>
-        </div>
-        <button type="button" class="btn btn-primary" name="search">
-            <i class="fas fa-search"></i>
-        </button>
+        <nav class="navbar navbar-light bg-light">
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        </nav>
         </div>
             <div class="card" style="width: 18rem;">
            
