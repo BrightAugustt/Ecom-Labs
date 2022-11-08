ac3234c1ed8b302221453ee0134a534fa970d59b
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-11-08 14:39:05 +0000

    added

--- actions/addproductprocess.php
+++ actions/addproductprocess.php
@@ -17,27 +17,47 @@ if(isset($_POST["addproduct"])){
     $image = $_FILES['product_image']["name"];
     $tmp = $_FILES['product_image']["tmp_name"];
 
+    // Storing image
+    $imgExt = explode('.',$image);
+    $imgActualExt = strtolower(end($imgExt));
+    $allowed = array('jpg','jpeg','png');
+    $imgError = $_FILES['product_image']['error'];
+    $imgSize = $_FILES['product_image']['size'];
 
-    function upload($directory,$subdir,$tempname,$image){
-
-        $folder = "../$directory/$subdir/".$image;
-
-        if(!file_exists("../$directory/$subdir/")){
-            // Create a new directory if file does not exist
-            @mkdir("../$directory/$subdir/",0777);
-            echo("New folder created");
-            move_uploaded_file($tempname,$folder);
-            return $folder;
-        }else{
-            move_uploaded_file($tempname,$folder);
-            return $folder;
+    if(in_array($imgActualExt,$allowed))
+    {
+        if($imgError === 0){
+            if($imgSize < 1000000 ){
+                $fileNameNew = uniqid('',true).".".$imgActualExt;
+                $fileDestination = '../images/upload'.$fileNameNew;
+                move_uploaded_file($tmp,$fileDestination);
+                header('Location:../Admin/view.php?uploadsuccess');
+            }
         }
-        return false;
-        }
+    }else{
+        echo "Upload failed, check image format!";
+    }
+
+    // function upload($directory,$subdir,$tempname,$image){
+
+    //     $folder = "../$directory/$subdir/".$image;
+
+    //     if(!file_exists("../$directory/$subdir/")){
+    //         // Create a new directory if file does not exist
+    //         @mkdir("../$directory/$subdir/",0777);
+    //         echo("New folder created");
+    //         move_uploaded_file($tempname,$folder);
+    //         return $folder;
+    //     }else{
+    //         move_uploaded_file($tempname,$folder);
+    //         return $folder;
+    //     }
+    //     return false;
+    //     }
 
 
     
-    $product_image = upload("Images","product",$tmp,$image);
+    // $product_image = upload("Images","product",$tmp,$image);
     // Call a controller
     addProducts_ctr($product_cat, $product_brand,$product_title,$product_price,$product_desc,$product_image,$product_keywords);
 
