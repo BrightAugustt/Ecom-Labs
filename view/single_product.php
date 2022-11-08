6587bbfc1337cc61be8c45115c62b42f0afe37fe
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-11-08 20:51:57 +0000

    aa

--- view/single_product.php
+++ view/single_product.php
@@ -19,7 +19,7 @@ if (empty($_SESSION['customer_id']) and empty($_SESSION['customer_name']) and em
 </head>
 <body>
     <?php
-    $product =  selectoneproduct_ctr($product_id); 
+    $product =  selectall_product_ctr(); 
     
     echo $product['product_title'];
 
