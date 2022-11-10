f7a3304310b95f7b12eab49703420226dabc70e3
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-11-10 21:22:58 +0000

    aa

--- view/all_product.php
+++ view/all_product.php
@@ -22,18 +22,16 @@ include("../functions/add_to_cart.php");
  
 <!-- Bootstrap -->
 
-        
+        <?php 
+           $product =  selectall_product_ctr();  
+            foreach($product as $item){ 
+        ?>
         <nav class="navbar navbar-light bg-light">
         <form class="form-inline" action="../functions/searc_results.php" method="POST">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="submit">Search</button>
         </form>
         </nav>
-
-        <?php 
-           $product =  selectall_product_ctr();  
-            foreach($product as $item){ 
-        ?>
 
         <!-- <form action="../functions/add_to_cart.php" method="POST"> -->
             <div class="card" style="width: 18rem;">
