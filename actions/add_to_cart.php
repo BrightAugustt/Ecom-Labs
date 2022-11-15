4413c2773c15abfaaca6058a58fdddac0d682800
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-11-15 14:24:48 +0000

    aaa

--- actions/add_to_cart.php
+++ actions/add_to_cart.php
@@ -7,10 +7,10 @@ if(isset($_POST['addToCart'])){
 
    $p_id = $_POST['p_id'];
    $cid = $_SESSION['customer_id'];
-   $ip_add = $_SERVER["REMOTE_ADDR"];
    $qty = $_POST['qty'];
     
-   addcart_ctr($p_id,$ip_add,$c_id,$qty);
+   addcart_ctr($p_id,$_SERVER['REMOTE_ADDR'],$c_id,$qty);
+
    header('Location: ../view/cart.php');
 }
 
